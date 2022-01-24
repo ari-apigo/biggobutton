41ed782fed133880731ef0f3d32a1e2f88f11ba5
ari-apigo <ari.apigo125@gmail.com>
2022-01-23 23:52:54 -0800

    working w/o extra credit

--- app/src/main/java/edu/uw/biggobutton/MainActivity.kt
+++ app/src/main/java/edu/uw/biggobutton/MainActivity.kt
@@ -2,10 +2,29 @@ package edu.uw.biggobutton
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var btn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn = findViewById<Button>(R.id.button)
+
+        var count = 0
+        var s = ""
+        btn.setOnClickListener {
+            count += 1
+
+            if (count > 1) {
+                s = "s"
+            }
+
+            btn.setText("You have pushed me $count time$s!")
+        }
     }
 }
